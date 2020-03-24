d6ddb4136e900307facf0673f3a0f1ad3b127cfe
JeffRThomas <thomas.jeff@live.com>
2020-03-24 13:47:28 -0500

    App.js Clean Out & AppContainer Roll Call

---
+++ cw-app/src/Components/AppContainer.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Fragment} from 'react';
+
+class AppContainer extends Component {
+    constructor(props){
+        super(props);
+    };
+
+    render(){
+        return(
+            <Fragment>
+                AppContainer is present
+            </Fragment>
+        )
+    };
+};
+
+export default AppContainer;
