4477e518c9fc606fef6f4ad815334124888ba99f
JeffRThomas <thomas.jeff@live.com>
2020-03-24 14:04:01 -0500

    Fetch Method Error!!!

--- cw-app/src/Components/StarWarsCharacters.jsx
+++ cw-app/src/Components/StarWarsCharacters.jsx
@@ -6,11 +6,22 @@ class StarWarsCharacters extends Component {
         super(props);
     };
 
+    componentDidMount() {
+        getSomeStuff();
+    };
+
+    getSomeStuff = async () => {
+        let SomeStuff = await fetch('https://swapi.co/api/people/');
+        let StarWarsStuff = await SomeStuff.json();
+        console.log(StarWarsStuff);
+        console.table(StarWarsStuff);
+    };
+
     render() {
         return (
             <Fragment>
                 {/* Component Roll Call */}
-                StarWarsCharacters is present
+                {/* StarWarsCharacters is present */}
             </Fragment>
         )
     };
